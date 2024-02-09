879e364c70cfd71f8f7ffa004b37bfc1a8a76e4c
Trương Ngọc Khánh <112161377+TNK-ADMIN@users.noreply.github.com>
2024-02-09 23:15:21 +0700

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,8 @@
+from TNKDEV.info import __github__, __facebook__, __telegram__, __TG__, __zalo__, __age__
+
+print(__github__())
+print(__facebook__())
+print(__telegram__())
+print(__TG__())
+print(__zalo__())
+print(__age__())
